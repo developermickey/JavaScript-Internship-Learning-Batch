c7c8bf8906194c60e606de32ac5220471576b871
developermickey <mukesh.quazma@gmail.com>
2024-09-13 19:52:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,108 @@
+// Programming languages // Script Language
+
+// ise hota kya hai Fontend ke Kam me ati hai lekin Node JS Backend ke kam me bhi aati hai
+
+// variable , - done
+// datatypes , - done
+// operators , -
+// if else, loops, fucntions , array, events,  dom
+
+// syntex
+
+// var a; // initialization
+// a = 10; // declaration
+
+// console.log(a);
+
+// var name = "mukesh pathak";
+// var name = "mukesh pathak";
+
+// let name = "Mukesh pathak";
+// name = "Ramesh pathak";
+// name = "Roshan";
+
+// const PI = 3.14;
+
+// console.log(PI);
+
+// Variables End  var , let, const //
+
+// Data Types
+
+// 2 type ke hote hai
+// Primitive Data Types
+// 1. Number
+// 2. String - Done
+// 3. Boolean
+// 4. undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+// Non-primitive Data Types - Reference Types
+// Methods (), Objects {}, Array []
+
+// 1. String - Duniya me jo bhi text ho wo sab string hai
+
+// let name = "roshan";
+// let fullName = "Salman Khan";
+
+// jab bhi apko kisi me variable ke type ko janna ho to of use karo typesof
+// console.log(name);
+// console.log(typeof name);
+// console.log(fullName);
+// console.log(typeof fullName);
+
+// Number - Jo bhi integer and Float , decimal value ho wo sab number hai
+
+// let age = 26.508098098984598085454058769;
+
+// console.log(age);
+// console.log(typeof age);
+
+// Boolean - true or false value hai
+
+// let isStudentPass = true;
+
+// console.log(isStudentPass);
+// console.log(typeof isStudentPass);
+
+// undefined -
+// jab apne koi variable create kiya hai lekin uske koi value nahi di jab wo ek undefined value hoti it mean ki value exist hi nhi karti hai;
+
+// let a;
+
+// console.log(a);
+// console.log(typeof a);
+
+// Null -
+// jab apne koi variable create kiya hai lekin uske value null di jayega
+
+// let b = null;
+
+// console.log(b);
+// console.log(typeof b);
+
+// Arithmetic Operators
+// +, -, *, / , % , ++, --
+
+// Add,
+
+// let a = 13;
+// let b = 3;
+
+// let sum = a % b; // value operand and + operator
+
+// console.log(sum);
+
+let a = 10;
+// increment Operators
+// pre
+// console.log(++a);
+// post
+console.log(a++);
+console.log(a);
+
+// Sub, Mul, Div
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
