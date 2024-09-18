a5add79474bf96e1c9af10fa4e3dba18b1868b03
developermickey <mukesh.quazma@gmail.com>
2024-09-18 20:45:15 +0530

    Operators Added

--- script.js
+++ script.js
@@ -94,15 +94,115 @@
 
 // console.log(sum);
 
-let a = 10;
+// let a = 10;
 // increment Operators
 // pre
 // console.log(++a);
 // post
-console.log(a++);
-console.log(a);
+// console.log(a++);
+// console.log(a);
 
 // Sub, Mul, Div
 // Assignment Operators
+
+// let s;
+
+// s += t; // s = s + t;
+// s -= t; // s = s - t;
+// s *= t; // s = s * t;
+// s /= t; // s = s / t;
+// s %= t; // s = s % t;
+
+// let a = 10;
+
 // Comparison Operators
-// Logical Operators
+
+// ==,!=,  ===,!==, >, >=, <, <=
+
+// equality operator ==
+// Double Equals operator
+
+// let z = 10; // number
+// let y = "10"; // string
+// console.log(z == y); //  true
+
+// Inequality Operators
+// Not Equals operator
+
+// console.log(z != y); // false   // strictly check nahi karta hai
+
+//  === Strict Equality operator
+
+// let a = "name";
+// let b = "names";
+
+// console.log(a === b); // true
+
+//  === Strict Inequality operator
+
+// let a = "name";
+// let b = "name";
+
+// console.log(a !== b); // true
+
+// Greater than operator >
+
+// let x = 10;
+// let y = 40;
+
+// console.log(y > x); // false
+
+// Greater Equals than operator >
+
+// let x = 10;
+// let y = 40;
+
+// console.log(y >= x);
+
+// Less than operator <
+
+// let x = 10;
+// let y = 40;
+
+// console.log(x <= y);
+
+// // Logical Operators  MMMMMMMMM IMPORTANT
+
+// AND Operator &&  // dono Condition True Ho Tab Hum AND Operator ka use karte hai
+
+// apko exam me baithna hai to 2 condition hai kya 1. 75% Attendece , 2. apne fees jama ki ho
+
+// let fees = true;
+// let att = 75;
+
+// console.log(fees && att >= 75);
+
+// OR Operator || koi bhi ek Condition ka True hona jaruri hai tab jakar ap OR operator ka use kate ho
+
+// let studentPass = false;
+// let studentAtt = 76;
+
+// console.log(studentPass || studentAtt >= 75);
+
+// NOT Operator !  == Jo Bhi Value ho use ulta kar do  true to use false if false to use true
+
+// let studentPass = false;
+
+// console.log(!studentPass);
+// studentPass = true;
+// console.log(!studentPass);
+
+// if else and else if  // loops / object / funcation / array uski method push  pop shift unshit map filter reduce slice   / String uski method length, uppercase, lowercase , include, slice, trim
+// DOM events click, mouseenter, mouseleave, doubleclick,
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+let sum = (a, b) => {
+  return a + b;
+};
+
+let newSum = sum(10, 20);
+
+console.log(newSum);
